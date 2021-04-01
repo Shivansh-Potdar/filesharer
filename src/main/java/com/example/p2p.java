4afee2b87fa8d83475f2ba19535b3d80cdf35a86
Shivansh-Potdar <loldude482@gmail.com>
2021-04-01 23:04:56 +0530

    add:Main

--- src/main/java/com/example/p2p.java
+++ src/main/java/com/example/p2p.java
@@ -1,6 +1,7 @@
 package com.example;
 
 //Imports
+
 import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
@@ -15,13 +16,9 @@ import java.util.Scanner;
 public class p2p {
     //main
     public static void main(String[] args) {
-        new Thread(() ->{
-            new Server().accept();
-        }).start();
+        new Thread(() -> new Server().accept()).start();
 
-        new Thread(()->{
-            new Client().connect();
-        }).start();
+        new Thread(()-> new Client().connect()).start();
     }
 
     //Server Side
@@ -31,7 +28,52 @@ public class p2p {
         }
 
         void accept(){
+            int port = 8080;
 
+            try (ServerSocket serverSocket = new ServerSocket(port, 0, InetAddress.getByName("0.0.0.0"))) {
+
+                System.out.println("Server is listening on port " + port);
+
+                boolean active = true;
+
+                while (active) {
+                    try {
+                        //Main setup
+                        Socket socket = serverSocket.accept();
+
+                        System.out.println("New client connected");
+
+                        OutputStream output = socket.getOutputStream();
+                        PrintWriter writer = new PrintWriter(output, true);
+
+                        writer.println(new Date().toString());
+
+                        //Set up Reader
+                        InputStream input = socket.getInputStream();
+                        BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+
+                        //Send echo
+                        String echoText = socket.getInetAddress().getHostName()+":\n"+converter(reader.readLine());
+                        System.out.println(echoText);
+
+                        //Exit code
+                        if(reader.readLine() != null){
+                            String exitVal = reader.readLine();
+
+                            if (exitVal.contentEquals("exit"))
+                                active = false;
+                        }
+
+                    }catch (Exception e){
+                        e.printStackTrace();
+                        System.out.println(e.getMessage());
+                    }
+                }
+
+            } catch (Exception ex) {
+                System.out.println("Server exception: " + ex.getMessage());
+                ex.printStackTrace();
+            }
         }
     }
 
@@ -94,7 +136,6 @@ public class p2p {
     public static void downloader(String message) throws IOException {
         //Make file
         File file = new File(message);
-        var fileOutputStream = new FileOutputStream(file);
 
         var fileBytes = Files.readAllBytes(file.toPath());
 
@@ -106,4 +147,9 @@ public class p2p {
     public static String tob64(String s){
         return Base64.getEncoder().encodeToString(s.getBytes(StandardCharsets.UTF_8));
     }
+
+    static String converter (String s) {
+        return new String(Base64.getDecoder().decode(s));
+    }
+
 }
