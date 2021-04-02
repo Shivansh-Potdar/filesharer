5ee73fb67c740604f73d56b3b6b4a5b5781aaaa5
Shivansh-Potdar <loldude482@gmail.com>
2021-04-03 03:23:07 +0530

    added:Downloader

--- src/main/java/com/example/p2p.java
+++ src/main/java/com/example/p2p.java
@@ -5,11 +5,9 @@ import java.io.*;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.net.UnknownHostException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.util.Base64;
-import java.util.Date;
 import java.util.Scanner;
 
 public class p2p {
@@ -25,21 +23,10 @@ public class p2p {
             String choice = inp.next();
 
             if (choice.contains("S".toLowerCase()))
-                new Thread(() -> new Server().accept()).start();
+               new Server().accept();
 
             if (choice.contains("R".toLowerCase()))
-                new Thread(() -> {
-                    try {
-                        new Client().connect();
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                        try {
-                            new Client().reconnect();
-                        } catch (InterruptedException interruptedException) {
-                            interruptedException.printStackTrace();
-                        }
-                    }
-                }).start();
+                new Client().connect();
         };
 
 
@@ -52,7 +39,7 @@ public class p2p {
         void accept(){
             int port = 8080;
 
-            try (ServerSocket serverSocket = new ServerSocket(port, 0, InetAddress.getByName("0.0.0.0"))) {
+            try (ServerSocket serverSocket = new ServerSocket(port, 1, InetAddress.getByName("0.0.0.0"))) {
 
                 System.out.println("Server is listening on port " + port);
 
@@ -73,20 +60,29 @@ public class p2p {
 
                         sender(filePath);
 
+                        writer.println(sender(filePath));
+
                         //Set up Reader
                         InputStream input = socket.getInputStream();
                         BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+                        System.out.println(converter(reader.readLine()));
 
                     }catch (Exception e){
                         e.printStackTrace();
                         System.out.println(e.getMessage());
+                        reload();
                     }
                 }
 
             } catch (Exception ex) {
                 System.out.println("Server exception: " + ex.getMessage());
                 ex.printStackTrace();
+                reload();
             }
+        }
+
+        void reload(){
+            accept();
         }
     }
 
@@ -105,9 +101,11 @@ public class p2p {
                 InputStream input = socket.getInputStream();
                 BufferedReader reader = new BufferedReader(new InputStreamReader(input));
 
-                String time = reader.readLine();
+                String fileR = reader.readLine();
 
-                System.out.println(time);
+                System.out.println(fileR);
+
+                download(fileR);
 
                 //Send reply
                 OutputStream output = socket.getOutputStream();
@@ -116,8 +114,8 @@ public class p2p {
                 String reply =
                         """
                             ____________________________
-                            |Time received.             |
-                            |Decoded encoded time       |
+                            |File received.             |
+                            |Decoded encoded file       |
                             `````````````````````````````
                         """;
                 writer.println(Base64.getEncoder().encodeToString(reply.getBytes(StandardCharsets.UTF_8)));
@@ -138,9 +136,20 @@ public class p2p {
             connect();
         }
 
+        void download(String s) throws FileNotFoundException {
+            var fileBytes = Base64.getDecoder().decode(s.getBytes(StandardCharsets.UTF_8));
+
+            try (var stream = new FileOutputStream("src\\main\\resources\\output\\final")) {
+                stream.write(fileBytes);
+                System.out.println("File saved");
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 
-    public static void sender(String message) throws IOException {
+    public static String sender(String message) throws IOException {
         //Make file
         File file = new File(message);
 
@@ -151,10 +160,8 @@ public class p2p {
 
         System.out.println("File sent: "+base64File);
         System.out.println("File type: "+Files.probeContentType(file.toPath()));
-    }
 
-    public static String tob64(String s){
-        return Base64.getEncoder().encodeToString(s.getBytes(StandardCharsets.UTF_8));
+        return base64File;
     }
 
     static String converter (String s) {
